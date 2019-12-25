6de8bfca627269eabc20ad01ef3daf84898532ce
Isaac Vongphakdy <IVongphakdy@gmail.com>
2019-12-24 17:59:10 -0800

    email validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,20 @@
-const imgWarning = document.querySelector('.warning-img');
-const txtWarning = document.querySelector('.warning');
-const button = document.querySelector('.mainBtn');
+const form = document.querySelector('.main-form');
+// var email = document.getElementById('email');
 
-button.addEventListener("click", (e) => {
-    e.preventDefault();
-    imgWarning.classList.remove("none");
-    txtWarning.classList.remove("none");
+function validateEmail(emailField) {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    return reg.test(emailField);
+ }
+
+form.addEventListener("submit", (e) => {
+    const field = form.querySelector('input');
+    const txtWarning = form.querySelector('.warning');
+    const imgWarning = form.querySelector('.warning-img');
+
+    if (field.value.trim() == '' || !validateEmail(field.value)) {
+        e.preventDefault();
+        imgWarning.classList.remove("none");
+        txtWarning.classList.remove("none");
+    } 
 })
